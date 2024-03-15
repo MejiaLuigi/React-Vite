f943357ac963da2de26575a400a9dfa559b07caf
Luis Mejia <hoyosmejia14o6@gmail.com>
2024-03-14 20:52:35 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>
+          Hola Mundo
+        </p>
+      </div>
+      <p className="read-the-docs">
+       Soy Luis Mejia
+      </p>
+    </>
+  )
+}
+
+export default App
